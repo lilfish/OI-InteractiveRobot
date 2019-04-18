b8b40bdd190bffe741c265e8de99a46a1eb3f123
ryanvrosch <ryanvrosch@hotmail.com>
2019-04-18 11:56:03 +0000

    added Readme to MovingHead_COM
    deleted compiler /hex files.

--- Code/MovingHead_COMtoDMX_module/MH_SerialControl/Debug/VM_DBG/VM_mem_check_sam.cpp.d
+++
@@ -1,2 +0,0 @@
-C:\Users\ryanv\AppData\Local\Temp\VMBuilds\MH_SerialControl\uno\Debug\VM_DBG\VM_mem_check_sam.cpp.o: \
- c:\program\ files\ (x86)\microsoft\ visual\ studio\2017\enterprise\common7\ide\extensions\sivbitce.zrh\Micro\ Platforms\default\debuggers\VM_DBG\VM_mem_check_sam.cpp
