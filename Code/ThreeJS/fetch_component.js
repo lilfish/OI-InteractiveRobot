8c93ce06bf61fe9ead3dfd85ed56bc4e2febce8a
Fish <vincentvenhuizen1997@gmail.com>
2019-06-13 19:39:17 +0200

    Added mood flow
    Animations will be triggered by mood or detection

--- Code/ThreeJS/fetch_component.js
+++ Code/ThreeJS/fetch_component.js
@@ -7,7 +7,7 @@ function fetchData() {
         })
         .then(function (response) {
             console.log(response.status); // returns 200
-            if (errors > 0){
+            if (errors > 0) {
                 to_neutraal();
                 errors = 0;
             }
@@ -17,8 +17,8 @@ function fetchData() {
             console.log(myJson);
             if (myJson.posx && myJson.posy && myJson.my_width && myJson.my_height)
                 look(myJson.posx, myJson.posy, myJson.my_width, myJson.my_height);
-            if (myJson.emotion)
-                changeMood(myJson.emotion);
+            if (myJson.emotion, myJson.id)
+                changeMood(myJson.emotion, myJson.id);
         })
         .catch(function (error) {
             console.error(error);
@@ -32,11 +32,108 @@ function fetchData() {
 
 
 setInterval(() => {
-        fetchData();
+    fetchData();
 }, 1000);
 
-function changeMood(mood) {
-    console.log(mood);
+/*
+Emotions:
+    to_neutraal
+    to_happy
+    to_sad
+    to_ultraSad
+    to_angry
+    to_surprised
+Moods:
+    neutral
+    happy
+    sad
+    surprise
+    anger
+*/
+// variable om een id te onthouden en te kijken hoevaak iemand is gedetecteerd
+var old_id = "empty";
+var times_detected = 0;
+
+function changeMood(mood, person) {
+    // om een random animatie te kiezen op een emotie
+    var random3 = Math.floor(Math.random() * 3);
+    var random2 = Math.floor(Math.random() * 2);
+
+    // kijken hoevaak iemand als is gedetecteerd (1x = 1 seconde)
+    if (person = '-1') {
+        old_id = "empty";
+        times_detected = 0;
+        to_neutraal();
+    } else if (old_id == person) {
+        times_detected += 1;
+    } else {
+        old_id = person;
+        times_detected = 1;
+    }
+
+    // mood neutraal
+    if (mood == "neutral" && times_detected < 5) {
+        switch (random3) {
+            case 1:
+                //neutraal blijven
+                to_neutraal();
+                break;
+            case 2:
+                //suprised kijken
+                to_surprised();
+                break;
+            default:
+                //blij kijken
+                to_happy();
+        }
+    }
+
+    // mood blij
+    if (mood == "happy" && times_detected < 5) {
+        if (random2 == 0) {
+            to_surprised();
+        } else if (random2 == 1) {
+            to_happy();
+        }
+    }
+
+    // mood boos
+    if (mood == "anger" && times_detected < 5) {
+        switch (random3) {
+            case 1:
+                //neutraal blijven
+                to_sad();
+                break;
+            case 2:
+                //suprised kijken
+                to_surprised();
+                break;
+            default:
+                //blij kijken
+                to_happy();
+        }
+    }
+
+    // mood surprise
+    if (mood == "surprise" && times_detected < 5) {
+        if (random2 == 0) {
+            to_surprised();
+        } else if (random2 == 1) {
+            to_happy();
+        }
+    }
+
+    // mood sad
+    if (mood == "sad" && times_detected < 5){
+        to_ultraSad();
+    }
+
+    // robot word ongemakkelijk
+    if (times_detected > 15) {
+        to_ultraSad();
+    } else if (times_detected > 5) {
+        to_neutraal();
+    }
 }
 
 function look(posx, posy, win_width, win_height) {
